780605eacd88d76a7dff205d845c3781a4165ebe
Alexander Winter <a.w1nter@hotmail.com>
2016-01-16 13:05:21 -0500

    GDX project created !

---
+++ desktop/src/eu/spiritplayers/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package eu.spiritplayers.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import eu.spiritplayers.StoneGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StoneGame(), config);
+	}
+}
---
+++ html/src/eu/spiritplayers/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package eu.spiritplayers.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import eu.spiritplayers.StoneGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new StoneGame();
+        }
+}
